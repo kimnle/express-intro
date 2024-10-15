7c1a07a1e9d66083079a0d7ca00c80c63b08d956
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-15 20:48:09 +1100

    fix: better naming of router exports

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ app.post("/", (request, response) => {
     });
 });
 
-const {router} = require("./controllers/pokemonController.js");
+const PokemonController = require("./controllers/pokemonController.js");
 // localhost:3000/pokemon/
-app.use("/pokemon", router);
+app.use("/pokemon", PokemonController);
 
 const PORT = 3000;
 
