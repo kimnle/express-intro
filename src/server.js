f35ab7f7d90f667cfabb4e57786a8d95121b3668
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-15 20:58:53 +1100

    feat: project rearranged for test purposes

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+// Make an instance of the Express system
+// so we can configure it
+// eg. routes, settings
+const app = express();
+
+// GET localhost:3000
+// .get("/", (req, res) => {})
+// .get("/", (banana, elephant) => {})
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello world!!</h1>");
+    response.json({
+        message: "Hello world!!"
+    });
+});
+
+app.post("/", (request, response) => {
+    response.json({
+        message: "POST request received!!"
+    });
+});
+
+const PokemonController = require("./controllers/pokemonController.js");
+// localhost:3000/pokemon/
+app.use("/pokemon", PokemonController);
+
+module.exports = {
+    app
+}
